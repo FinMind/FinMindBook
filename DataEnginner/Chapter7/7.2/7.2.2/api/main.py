f0b9eaded8f9403608f740a50d2c1cf80493dd3b
sam <sam0527.lin@sinopac.com>
2021-04-19 17:05:36 +0800

    feat: api docker

--- DataEnginner/Chapter7/7.2/7.2.2/api/main.py
+++ DataEnginner/Chapter7/7.2/7.2.2/api/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from fastapi import FastAPI
 from sqlalchemy import create_engine, engine
+from api import config
 
 
 def get_mysql_financialdata_conn() -> engine.base.Connection:
-    address = "mysql+pymysql://root:test@127.0.0.1:3306/FinancialData"
+    address = (
+        f"mysql+pymysql://{config.MYSQL_DATA_USER}:{config.MYSQL_DATA_PASSWORD}"
+        f"@{config.MYSQL_DATA_HOST}:{config.MYSQL_DATA_PORT}/{config.MYSQL_DATA_DATABASE}"
+    )
     engine = create_engine(address)
     connect = engine.connect()
     return connect
