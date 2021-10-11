83c777096de094611af2d7c39eb19a4cb534eea0
sam <samlin266118@gmail.com>
2021-10-11 13:15:00 +0800

    feat: add sql

--- DataEnginner/Chapter11/11.5/upload_data2mysql.py
+++ DataEnginner/Chapter11/11.5/upload_data2mysql.py
@@ -1,4 +1,3 @@
-import pymysql
 import pandas as pd
 import io
 import sys
@@ -11,24 +10,103 @@ import requests
 
 
 def get_mysql_financialdata_conn() -> engine.base.Connection:
-    address = f"mysql+pymysql://root:test@139.162.104.54:3306/FinancialData"
+    # TODO 請將 IP 換成讀者自己的 IP
+    address = "mysql+pymysql://root:test@139.162.104.54:3306/FinancialData"
     engine = create_engine(address)
     connect = engine.connect()
     return connect
 
 
-def read_sql_file(table: str) -> str:
-    fd = open(f"./sql/{table}.sql", "r")
-    sql = fd.read()
-    fd.close()
-    return sql
+def create_taiwan_stock_info_sql():
+    return """
+        CREATE TABLE `taiwan_stock_info` (
+            `industry_category` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `stock_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `stock_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+            `type` varchar(4) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '上市twse/上櫃tpex',
+            `date` date DEFAULT NULL,
+            PRIMARY KEY (`stock_id`,`industry_category`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+    """
+
+
+def create_taiwan_stock_price_sql():
+    return """
+        CREATE TABLE `taiwan_stock_price` (
+            `stock_id` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+            `Trading_turnover` bigint NOT NULL,
+            `Trading_Volume` int NOT NULL,
+            `Trading_money` bigint NOT NULL,
+            `open` float NOT NULL,
+            `max` float NOT NULL,
+            `min` float NOT NULL,
+            `close` float NOT NULL,
+            `spread` float NOT NULL,
+            `date` date NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci
+        PARTITION BY KEY (stock_id)
+        PARTITIONS 10;
+    """
+
+
+def create_taiwan_stock_institutional_investors_sql():
+    return """
+        CREATE TABLE taiwan_stock_institutional_investors(
+            `name` VARCHAR(20),
+            `buy` BIGINT(64),
+            `sell` BIGINT(64),
+            `stock_id` VARCHAR(10),
+            `date` DATE,
+            PRIMARY KEY(`stock_id`, `date`, `name`)
+        ) PARTITION BY KEY(`stock_id`) PARTITIONS 10;
+    """
+
+
+def create_taiwan_stock_margin_purchase_short_sale_sql():
+    return """
+        CREATE TABLE `taiwan_stock_margin_purchase_short_sale`(
+            `stock_id` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '股票代碼',
+            `MarginPurchaseBuy` BIGINT NOT NULL COMMENT '融資買進',
+            `MarginPurchaseSell` BIGINT NOT NULL COMMENT '融資賣出',
+            `MarginPurchaseCashRepayment` BIGINT NOT NULL COMMENT '融資現金償還',
+            `MarginPurchaseYesterdayBalance` BIGINT NOT NULL COMMENT '融資昨日餘額',
+            `MarginPurchaseTodayBalance` BIGINT NOT NULL COMMENT '融資今日餘額',
+            `MarginPurchaseLimit` BIGINT NOT NULL COMMENT '融資限額',
+            `ShortSaleBuy` BIGINT NOT NULL COMMENT '融券買進',
+            `ShortSaleSell` BIGINT NOT NULL COMMENT '融券賣出',
+            `ShortSaleCashRepayment` BIGINT NOT NULL COMMENT '融券償還',
+            `ShortSaleYesterdayBalance` BIGINT NOT NULL COMMENT '融券昨日餘額',
+            `ShortSaleTodayBalance` BIGINT NOT NULL COMMENT '融券今日餘額',
+            `ShortSaleLimit` BIGINT NOT NULL COMMENT '融券限制',
+            `OffsetLoanAndShort` BIGINT DEFAULT NULL COMMENT '資券互抵',
+            `date` DATE NOT NULL COMMENT '日期',
+            PRIMARY KEY(`stock_id`, `date`)
+        ) PARTITION BY KEY(`stock_id`) PARTITIONS 10;
+    """
+
+
+def create_taiwan_stock_holding_shares_per_sql():
+    return """
+        CREATE TABLE taiwan_stock_holding_shares_per (
+            `HoldingSharesLevel` VARCHAR(19),
+            `people` INT(10),
+            `unit` BIGINT(64),
+            `percent` FLOAT,
+            `stock_id` VARCHAR(10),
+            `date` DATE,
+            `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`stock_id`,`date`,`HoldingSharesLevel`)
+        )
+        PARTITION BY KEY(stock_id)
+        PARTITIONS 10;
+    """
 
 
 def create_table(
     table: str,
     mysql_conn: engine.base.Connection,
 ):
-    sql = read_sql_file(table)
+    sql = eval(f'create_{table}_sql()')
     try:
         logger.info(
             f"create table {table}"
